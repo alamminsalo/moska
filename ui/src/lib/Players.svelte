70f8c8e34a9f8dbad34ec0fefe7a1ca96359082b
alamminsalo <0dosages-remits@icloud.com>
2024-07-09 22:47:00 +0300

    ts linting fix

--- ui/src/lib/Players.svelte
+++ ui/src/lib/Players.svelte
@@ -3,15 +3,15 @@
   import {Player} from './moska/moska';
 
   export let players: Player[];
-  export let current: Player;
+  export let current: Player | null;
 </script>
 
 <!-- player hands -->
 <div class="flex grow justify-end scale-inherit">
   {#each players as player}
     <div class="text-center">
-      <div class:active={current.id == player.id}>{"Player " + (player.id+1)}</div>
-      <Hand count={player.cards.length} open={current.id == player.id}/>
+      <div class:active={current?.id == player.id}>{"Player " + (player.id+1)}</div>
+      <Hand count={player.cards.length} open={current?.id == player.id}/>
       <div>x{player.cards.length}</div>
     </div>
   {/each}
